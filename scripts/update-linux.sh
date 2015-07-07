82b90316b968d077ccbcff671363751ddbd88270
StefanoBelli <sa.belli@hotmail.it>
2015-07-07 17:02:56 +0200

    scripts/update-linux.sh: added script (git)

---
+++ scripts/update-linux.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+update-linux(){
+		  CONF_FILE=".config"
+		  cd /home/stefanozzz123/kernels/git/linux
+		  printf "\033[32m*\033[0m Checking for .config file....\n"
+		  [[ -f $CONF_FILE ]] && 
+		  printf "\033[32m*\033[0m $CONF_FILE exists... \n"  
+		  sleep 1 || make menuconfig
+
+		  printf "\033[33m*\033[0m Fetching latest commits from remote repository (origin)...\n"
+		  git pull
+		  echo -n "? Would you like to continue[y/n]?-> "; read CHOICE
+
+		  case $CHOICE in
+					 ("y")
+								printf "\033[32m*\033[0m Compiling source...\n"
+								make -j3
+								printf "\033[32m*\033[0m Copying kernel...\n"
+								sudo cp arch/x86/boot/bzImage /boot/vmlinuz-linux_git_build
+								printf "\033[32m*\033[0m Updating GRUB...\n"
+								sudo grub2-mkconfig -o /boot/grub/grub.cfg
+								exit 0
+								break;;
+					 ("n")
+								printf "\033[31m*\033[0m Bye. "
+								exit 1;;
+					 (*)
+								echo "Non-valid choice..."
+								exit 2
+								break;;
+			esac
+ }
+update-linux
