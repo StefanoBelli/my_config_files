7c8c155a784f7233b71b21be0d9c1c365e2e951e
StefanoBelli <sa.belli@hotmail.it>
2015-12-24 02:44:49 +0100

    Changed fish

--- fish-shell_config/archlinux/fish/config.fish
+++ fish-shell_config/archlinux/fish/config.fish
@@ -1,3 +1,8 @@
+set KERN_VERSION (uname -r)
+set fish_greeting "Welcome back, $USER !
+Using kernel: $KERN_VERSION
+Fish version: $FISH_VERSION
+"
 set -x PATH $PATH
 set -x EDITOR "vim"
 set -x GCCFLAGS "-O3 -pipe -Wall"
