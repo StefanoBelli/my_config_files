1589202636a24a9a83206c5009815069db7f4114
StefanoBelli <sa.belli@hotmail.it>
2015-12-29 21:25:13 +0100

    Added some aliases to config.fish (archlinux)

--- fish-shell_config/archlinux/fish/config.fish
+++ fish-shell_config/archlinux/fish/config.fish
@@ -3,8 +3,11 @@ set fish_greeting "Welcome back, $USER !
 Using kernel: $KERN_VERSION
 Fish version: $FISH_VERSION
 "
-set -x PATH $PATH
+set -x PATH /home/stefanozzz123/.bin $PATH
 set -x EDITOR "vim"
 set -x GCCFLAGS "-O3 -pipe -Wall"
 set -x MAKEFLAGS "-j3"
 
+alias pissh "ssh 192.168.2.100 -p 22 -l "
+alias piscan "ssh 192.168.2.100 -p 22 -l root scan "
+alias pienablecups "ssh 192.168.2.100 -p 22 -l root cupsd "
