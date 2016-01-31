2377cde4bd545f06e450c8034f924444d62efe1a
StefanoBelli <sa.belli@hotmail.it>
2016-01-31 20:13:01 +0100

    added ranger, changed fish config

--- fish-shell_config/archlinux/fish/fish/config.fish
+++ fish-shell_config/archlinux/fish/fish/config.fish
@@ -5,7 +5,7 @@ Fish version: $FISH_VERSION
 "
 set -x LANG "it_IT.UTF-8"
 set -x PATH /home/stefanozzz123/.bin $PATH
-set -x EDITOR "vim"
+set -x EDITOR "emacs -nw"
 set -x GCCFLAGS "-O3 -pipe -Wall"
 set -x MAKEFLAGS "-j3"
 
@@ -16,6 +16,9 @@ alias tmuxnew "tmux new-session -c $HOME -n Main:fish -s "
 alias wanip "curl ipecho.net/plain"
 alias kbd "cat ~/.i3/config | grep "
 alias cemacs "emacs -nw "
+alias automount "udiskie-mount "
+alias autoumount "udiskie-umount "
+alias rangerhere "ranger ."
 
 #startx when login
 if status --is-login and test -e /etc/systemd/system/display-manager.service  
