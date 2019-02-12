533911e02b60264a456bf42685d8566e6391f569
StefanoBelli <sa.belli@hotmail.it>
2019-02-12 20:32:40 +0100

    lemonbar_launcher.sh automatically restarts lemonbar if script crashes

--- bspwm/lemonbar_launcher.sh
+++ bspwm/lemonbar_launcher.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-/home/ssynx/.config/bspwm/populate_bar.py | lemonbar -p -B "#2C2C2C" -f "monospace:size=10:antialias=true" &
+LEMONBAR=$(which lemonbar)
+SCRIPT=/home/ssynx/.config/bspwm/populate_bar.py
+
+while true; do
+    $SCRIPT | $LEMONBAR -B "#2C2C2C" -f "monospace:size=10:antialias=true"
+    echo -e "$SCRIPT crashed, restarting in 3 secs..." | $LEMONBAR -B "#2C2C2C" -f "monospace:size=10:antialias=true" -p &
+    sleep 3
+    killall lemonbar
+done
